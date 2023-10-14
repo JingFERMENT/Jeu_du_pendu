cf7e2728fe58a7e5b1fa9dc380cfc01aa7ca1a39
Jing ZHANG <jing.zhang.ferment@gmail.com>
2023-10-14 22:40:22 +0200

    algo done + html V1 + image prepared

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,65 +1,98 @@
-const words = ["ecole", "ordinateur", "telephone", "elise"];
+const words = ["ecole", "maison", "livre", "medecin","apprendre","developpeur", "javascript"];
 
 // 1) l'ordinateur choisit un mot aléatoire
 let aChosenWordByComputer = words[Math.floor(Math.random() * words.length)];
 console.log(aChosenWordByComputer);
-console.log(aChosenWordByComputer.length);
+// cibler l'endroit où mettre le mot choisi aléatoirement par l'ordinateur
+const displayRandomWord = document.getElementById("display_random_word");
+
+const btnGroup = document.getElementById("btn_group");
+
+const btnA = document.getElementById("btn_a");
+console.log(btnA.value);
+
+// transformer le mot en un tableau afin de rechercher chaque lettre
+const aChosenWordByComputerArray = aChosenWordByComputer.split("");
+console.log(aChosenWordByComputerArray);
 
 // 2) dessiner une rangée de tirets, qui correspondant au nombre des lettres de ce mot
-let nbOfDashes = aChosenWordByComputer.replace(/./g, '_');
-console.log(nbOfDashes);
+let nbOfStars = aChosenWordByComputer.replace(/./g, "*");
+console.log(nbOfStars);
+displayRandomWord.innerHTML = nbOfStars;
+
+// transformer les tirets en un tableau afin de rechercher chaque lettre
+const dashArray = nbOfStars.split("");
+// console.log(dashArray)
 
 // 3) le joueur annonce une lettre
-let aChosenLetterByPlayer = "e";
+const nbOfTrials = 7;
 
-// 4) La lettre fait-elle partie du mot ?
-if (aChosenWordByComputer.includes(aChosenLetterByPlayer)) {
-    //oui
-    console.log("Bien joué!")
-    
-    // transformer le mot en un tableau afin de rechercher chaque lettre
-    const aChosenWordByComputerArray = aChosenWordByComputer.split("")
-    console.log(aChosenWordByComputerArray);
-    
-    // transformer les tirets en un tableau afin de rechercher chaque lettre
-    const dashArray = nbOfDashes.split("");
-    // console.log(dashArray)
+// déclarer un tableau vide pour mettre toutes les lettres chosies par le joueur
+// mettre ces lettres dans le tableau
+let chosenlettersByPlayer = [];
+// let a = "e";
+// chosenlettersByPlayer.push(a);
 
-        let foundedLetterIndex = aChosenWordByComputerArray.indexOf(aChosenLetterByPlayer);
-        console.log(foundedLetterIndex);
-        
+console.log(chosenlettersByPlayer);
 
-        // remplacer le trait par une lettre réussie 
-        dashArray.splice(foundedLetterIndex, 1, aChosenWordByComputer[foundedLetterIndex]);
-        console.log(dashArray);
-        console.log(aChosenWordByComputer[foundedLetterIndex]);
-        console.log(foundedLetterIndex);
+// compteur: pour calculer le nombre des fois que le joueur a essayé
+let count = 0;
+
+//  4) vérifrier si les lettres choisies par les joueurs correspondent au mot choisir par l'ordinateur
+chosenlettersByPlayer.forEach((aChosenLetterByPlayer) => {
+    // si la lettre fait partie du mot 
+    if (aChosenWordByComputer.includes(aChosenLetterByPlayer)) {
+        let allIndexsCorresponding = getAllIndexes(
+            aChosenWordByComputer,
+            aChosenLetterByPlayer
+        );
+        // console.log(allIndexsCorresponding);
+
+        allIndexsCorresponding.forEach((oneIndexCorresponding) => {
+            // remplacer le trait par une lettre réussie
+            dashArray.splice(
+                oneIndexCorresponding,
+                1,
+                aChosenWordByComputer[oneIndexCorresponding]
+            );
+        });
         
-        function getAllIndexes(word, letter) {
-            let indexes = [];
-            for(let i = 0; i < word.length; i++)
-                if (word[i] === letter)
-                    indexes.push(i);
-            return indexes;
-        }
-        
-        console.log(getAllIndexes(aChosenWordByComputer, aChosenLetterByPlayer));
-        
-        
-    } else {
-    console.log("Raté!")
-    console.log(nbOfDashes);
-    //non, l'ordinateur dessine le premier trait du pendu
-    //afficher le premier dessin, afficher le 2ème dessin, le 3ème dessin etc ...
+    } else { // La lettre ne fait pas partie du mot 
+        count++
+        // l'ordinateur dessine le premier trait du pendu
+        //afficher le premier dessin, afficher le 2ème dessin, le 3ème dessin etc ...
+    }
+
+});
+
+let leftOfTrails = nbOfTrials - count;
+
+// console.log(dashArray);
+// console.log(chosenlettersByPlayer);
+// console.log((JSON.stringify(dashArray) == JSON.stringify(chosenlettersByPlayer)));
+
+if (count <= nbOfTrials && (JSON.stringify(dashArray) == JSON.stringify(aChosenWordByComputerArray))) {
+    console.log("Vous avez gagné le jeu.")
+    // console.log(dashArray);
+} // le joueur gagne la partie en trouvant toutes les lettrres du mot ou en le devinant correctement.
+else if (count < nbOfTrials) {
+    console.log(`Il vous reste ${leftOfTrails} essais.`);
+}
+else {
+    // l'ordinateur gagne la partie en complétant le dessin du pendu
+    console.log("Vous avez raté le jeu.")
+}
+
+// trouver tous les indexs correspondant dans un mot
+function getAllIndexes(word, letter) {
+    let indexes = [];
+    for (let index = 0; index < word.length; index++)
+        if (word[index] === letter) indexes.push(index);
+    return indexes;
 }
 
 
-
-// 6) le jeu se poursuit jusqu'à ce que
-// 7) le joueur gagne la partie en trouvant toutes les lettrres du mot ou en le devinant correctement.
-// 8) l'ordinateur gagne la partie en complétant le dessin du pendu
-
-// l'ordinateur l'inscrit à sa place autant de fois qu'elle se trouve dans le mot
-
+// écouteur d'évènement sur les boutons des lettres
+// btnGroup.addEventListener("click", checkLetter);
 
 
