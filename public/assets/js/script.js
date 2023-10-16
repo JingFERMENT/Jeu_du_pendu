68def851b843db70e6d9980cebccb9d08ade755a
Jing ZHANG <jing.zhang.ferment@gmail.com>
2023-10-16 16:06:39 +0200

    V2: ajout homepage + changer le style

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -9,6 +9,9 @@ const displayRandomWord = document.getElementById("display_random_word");
 // cible l'endroit où il y a toutes les boutons des lettres
 const btnGroup = document.getElementById("btn_group");
 // console.log(btnGroup)
+// const btnA = document.getElementById("btn_a");
+// btnA.disabled = true;
+// console.log(btnA);
 
 const firstImageOfPendu = document.getElementById("pendu_1");
 const secondImageOfPendu = document.getElementById("pendu_2");
@@ -18,13 +21,12 @@ const fifthImageOfPendu = document.getElementById("pendu_5");
 const sixthImageOfPendu = document.getElementById("pendu_6");
 const seventhImageOfPendu = document.getElementById("pendu_7");
 const helpInfo = document.getElementById("helpInfo");
+const cover1 = document.getElementById("cover1");
+const cover2 = document.getElementById("cover2");
+
+// console.log(cover);
 // console.log(helpInfo);
-
 // console.log(firstImageOfPendu);
-
-// const btnA = document.getElementById("btn_a");
-// console.log(btnA.value);
-
 // transformer le mot en un tableau afin de rechercher chaque lettre
 const aChosenWordByComputerArray = aChosenWordByComputer.split("");
 console.log(aChosenWordByComputerArray);
@@ -41,60 +43,6 @@ const starArray = nbOfStars.split("");
 // 3) le joueur annonce une lettre
 const nbOfTrials = 7;
 
-// déclarer un tableau vide pour mettre toutes les lettres chosies par le joueur
-// mettre ces lettres dans le tableau
-// let chosenlettersByPlayer = [];
-// // let a = "e";
-// // chosenlettersByPlayer.push(a);
-
-// console.log(chosenlettersByPlayer);
-
-
-// //  4) vérifrier si les lettres choisies par les joueurs correspondent au mot choisir par l'ordinateur
-// chosenlettersByPlayer.forEach((aChosenLetterByPlayer) => {
-//     // si la lettre fait partie du mot 
-//     if (aChosenWordByComputer.includes(aChosenLetterByPlayer)) {
-//         let allIndexsCorresponding = getAllIndexes(
-//             aChosenWordByComputer,
-//             aChosenLetterByPlayer
-//         );
-//         // console.log(allIndexsCorresponding);
-
-//         allIndexsCorresponding.forEach((oneIndexCorresponding) => {
-//             // remplacer le trait par une lettre réussie
-//             starArray.splice(
-//                 oneIndexCorresponding,
-//                 1,
-//                 aChosenWordByComputer[oneIndexCorresponding]
-//             );
-//         });
-
-//     } else { // La lettre ne fait pas partie du mot 
-//         count++
-//         // l'ordinateur dessine le premier trait du pendu
-//         //afficher le premier dessin, afficher le 2ème dessin, le 3ème dessin etc ...
-//     }
-
-// });
-
-// let leftOfTrails = nbOfTrials - count;
-
-// // console.log(starArray);
-// // console.log(chosenlettersByPlayer);
-// // console.log((JSON.stringify(starArray) == JSON.stringify(chosenlettersByPlayer)));
-
-// if (count <= nbOfTrials && (JSON.stringify(starArray) == JSON.stringify(aChosenWordByComputerArray))) {
-//     console.log("Vous avez gagné le jeu.")
-//     // console.log(starArray);
-// } // le joueur gagne la partie en trouvant toutes les lettrres du mot ou en le devinant correctement.
-// else if (count < nbOfTrials) {
-//     console.log(`Il vous reste ${leftOfTrails} essais.`);
-// }
-// else {
-//     // l'ordinateur gagne la partie en complétant le dessin du pendu
-//     console.log("Vous avez raté le jeu.")
-// }
-
 // // trouver tous les indexs correspondant dans un mot
 function getAllIndexes(word, letter) {
     let indexes = [];
@@ -108,14 +56,16 @@ let count = 0;
 btnGroup.addEventListener("click", (event) => {
 
     let aChosenLetterByPlayer = event.target.value;
+    // console.log(aChosenLetterByPlayer);
     // let chosenlettersByPlayer = [];
     // chosenlettersByPlayer.push(aChosenLetterByPlayer);
-    // console.log(aChosenLetterByPlayer);
+    
+    console.log(event.target.classList.add("d-none"));
+    
     // console.log(aChosenWordByComputer)
     // console.log(aChosenWordByComputer.includes(aChosenLetterByPlayer))
 
     // compteur: pour calculer le nombre des fois que le joueur a essayé
-    
 
     if (aChosenWordByComputer.includes(aChosenLetterByPlayer)) {
         let allIndexsCorresponding = getAllIndexes(aChosenWordByComputer, aChosenLetterByPlayer);
@@ -137,13 +87,11 @@ btnGroup.addEventListener("click", (event) => {
         //afficher le premier dessin, afficher le 2ème dessin, le 3ème dessin etc ...
     }
 
-
     let leftOfTrails = nbOfTrials - count;
     //
     let starArrayAsString = starArray.toString();
     starArrayAsString = starArrayAsString.replace(/,/g, '')
     displayRandomWord.innerHTML = starArrayAsString;
-    
 
     // console.log(starArray);
     // console.log(chosenlettersByPlayer);
@@ -151,7 +99,7 @@ btnGroup.addEventListener("click", (event) => {
 
     if (count === 1) {
         firstImageOfPendu.classList.remove("d-none");
-        secondImageOfPendu.classList.add("d-none"); 
+        secondImageOfPendu.classList.add("d-none");
         thirdImageOfPendu.classList.add("d-none");
         fourthImageOfPendu.classList.add("d-none");
         fifthImageOfPendu.classList.add("d-none");
@@ -177,37 +125,37 @@ btnGroup.addEventListener("click", (event) => {
         firstImageOfPendu.classList.add("d-none");
         secondImageOfPendu.classList.add("d-none");
         thirdImageOfPendu.classList.add("d-none");
-        fourthImageOfPendu.classList.remove("d-none"); 
+        fourthImageOfPendu.classList.remove("d-none");
         fifthImageOfPendu.classList.add("d-none");
         sixthImageOfPendu.classList.add("d-none");
         seventhImageOfPendu.classList.add("d-none");
-    } else if (count ===5) {
+    } else if (count === 5) {
         firstImageOfPendu.classList.add("d-none");
         secondImageOfPendu.classList.add("d-none");
         thirdImageOfPendu.classList.add("d-none");
-        fourthImageOfPendu.classList.add("d-none"); 
+        fourthImageOfPendu.classList.add("d-none");
         fifthImageOfPendu.classList.remove("d-none");
         sixthImageOfPendu.classList.add("d-none");
         seventhImageOfPendu.classList.add("d-none");
-    } else if (count ===6) {
+    } else if (count === 6) {
         firstImageOfPendu.classList.add("d-none");
         secondImageOfPendu.classList.add("d-none");
         thirdImageOfPendu.classList.add("d-none");
-        fourthImageOfPendu.classList.add("d-none"); 
+        fourthImageOfPendu.classList.add("d-none");
         fifthImageOfPendu.classList.add("d-none");
-        sixthImageOfPendu.classList.remove("d-none"); 
+        sixthImageOfPendu.classList.remove("d-none");
         seventhImageOfPendu.classList.add("d-none");
-    } else if (count ===7) {
+    } else if (count === 7) {
         firstImageOfPendu.classList.add("d-none");
         secondImageOfPendu.classList.add("d-none");
         thirdImageOfPendu.classList.add("d-none");
-        fourthImageOfPendu.classList.add("d-none"); 
+        fourthImageOfPendu.classList.add("d-none");
         fifthImageOfPendu.classList.add("d-none");
-        sixthImageOfPendu.classList.add("d-none"); 
+        sixthImageOfPendu.classList.add("d-none");
         seventhImageOfPendu.classList.remove("d-none");
     } else {
         firstImageOfPendu.classList.add("d-none");
-        secondImageOfPendu.classList.add("d-none"); 
+        secondImageOfPendu.classList.add("d-none");
         firstImageOfPendu.classList.add("d-none");
         firstImageOfPendu.classList.add("d-none");
         firstImageOfPendu.classList.add("d-none");
@@ -217,19 +165,78 @@ btnGroup.addEventListener("click", (event) => {
 
 
     if (count <= nbOfTrials && (JSON.stringify(starArray) == JSON.stringify(aChosenWordByComputerArray))) {
-        helpInfo.innerHTML = "Vous avez gagné le jeu.";
+        helpInfo.innerHTML = "Vous avez gagné le jeu. \ud83c\udf89 \uD83D\uDE4C";
         console.log(helpInfo.innerText);
+        const addCover2 = cover2.classList.remove("d-none");
+        console.log(addCover2);
+        // location.reload();
     } // le joueur gagne la partie en trouvant toutes les lettrres du mot ou en le devinant correctement.
     else if (count < nbOfTrials) {
-        helpInfo.innerHTML = `Il vous reste ${leftOfTrails} essais.`;
+        helpInfo.innerHTML = `Il vous reste ${leftOfTrails} essais. \u270A`;
         console.log(helpInfo.innerHTML);
     }
     else {
         // l'ordinateur gagne la partie en complétant le dessin du pendu
-        helpInfo.innerHTML = "Vous avez raté le jeu.";
+        helpInfo.innerHTML = "Vous avez raté le jeu.\uD83D\uDE22 ";
         console.log(helpInfo.innerHTML);
+        const addCover1 = cover1.classList.remove("d-none");
+        console.log(addCover1);
+        // location.reload();
     }
 
 });
 
 
+
+// déclarer un tableau vide pour mettre toutes les lettres chosies par le joueur
+// mettre ces lettres dans le tableau
+// let chosenlettersByPlayer = [];
+// // let a = "e";
+// // chosenlettersByPlayer.push(a);
+
+// console.log(chosenlettersByPlayer);
+
+// //  4) vérifrier si les lettres choisies par les joueurs correspondent au mot choisir par l'ordinateur
+// chosenlettersByPlayer.forEach((aChosenLetterByPlayer) => {
+//     // si la lettre fait partie du mot
+//     if (aChosenWordByComputer.includes(aChosenLetterByPlayer)) {
+//         let allIndexsCorresponding = getAllIndexes(
+//             aChosenWordByComputer,
+//             aChosenLetterByPlayer
+//         );
+//         // console.log(allIndexsCorresponding);
+
+//         allIndexsCorresponding.forEach((oneIndexCorresponding) => {
+//             // remplacer le trait par une lettre réussie
+//             starArray.splice(
+//                 oneIndexCorresponding,
+//                 1,
+//                 aChosenWordByComputer[oneIndexCorresponding]
+//             );
+//         });
+
+//     } else { // La lettre ne fait pas partie du mot
+//         count++
+//         // l'ordinateur dessine le premier trait du pendu
+//         //afficher le premier dessin, afficher le 2ème dessin, le 3ème dessin etc ...
+//     }
+
+// });
+
+// let leftOfTrails = nbOfTrials - count;
+
+// // console.log(starArray);
+// // console.log(chosenlettersByPlayer);
+// // console.log((JSON.stringify(starArray) == JSON.stringify(chosenlettersByPlayer)));
+
+// if (count <= nbOfTrials && (JSON.stringify(starArray) == JSON.stringify(aChosenWordByComputerArray))) {
+//     console.log("Vous avez gagné le jeu.")
+//     // console.log(starArray);
+// } // le joueur gagne la partie en trouvant toutes les lettrres du mot ou en le devinant correctement.
+// else if (count < nbOfTrials) {
+//     console.log(`Il vous reste ${leftOfTrails} essais.`);
+// }
+// else {
+//     // l'ordinateur gagne la partie en complétant le dessin du pendu
+//     console.log("Vous avez raté le jeu.")
+// }
